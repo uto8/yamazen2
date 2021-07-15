3a23e4000774fbbabe1d1ab4ad12d4924409aeaf
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2021-07-15 18:05:50 +0900

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,17 @@ $(".footer_btn").on("click",()=>{
 $(".contact_btn").on("click",()=>{
   location.href = "#section06";
 });
+let num=1;
+$(".btn-more").on("click",()=>{
+  num=num +1;
+  if((num%3)==1){
+    $(".hidden1").addClass("closed").removeClass("opened");
+    $(".hidden2").addClass("closed").removeClass("opened");
+    $(".btn-more").addClass("closed").removeClass("opened");
+  }else if((num%3)==2){
+    $(".hidden1").addClass("opened").removeClass("closed");
+  }else{
+    $(".hidden2").addClass("opened").removeClass("closed");
+    $(".btn-more").addClass("opened").removeClass("closed");
+  }
+})
